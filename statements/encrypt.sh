6106c7fce2696cb26dfc15ba1e9d093cb2da69bf
Di Xu <xudifsd@gmail.com>
2020-03-18 15:34:56 -0700

    change encrypt script

--- statements/encrypt.sh
+++ statements/encrypt.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
-USAGE="Usage $0 [en|de]"
+USAGE="Usage $0 [en|de] file"
 action=
 
 if [ "$#" -eq 0 ] ; then
     echo "Usage $0 [en|de]" >&2
     exit 1
-elif [ "$#" -eq 1 ] ; then
-    action=$1
 else
-    echo $USAGE >&2
-    exit 2
+    action=$1
 fi
 
 if [ "x$action" == "xen" ]; then
-    for i in `find . -type f -regex ".*.pdf"` ; do
-        prefix=${i%%".pdf"}
-        cat $i | gpg --default-recipient-self --armor --encrypt > $prefix.gpg
-        shred -u $i
-    done
+    if [ "$#" -eq 1 ] ; then
+        for i in `find . -type f -regex ".*.pdf"` ; do
+            prefix=${i%%".pdf"}
+            cat $i | gpg --default-recipient-self --armor --encrypt > $prefix.gpg
+            shred -u $i
+        done
+    else
+        echo $USAGE >&2
+        exit 2
+    fi
 elif [ "x$action" == "xde" ]; then
-    for i in `find . -type f -regex ".*.gpg"` ; do
-        prefix=${i%%".gpg"}
-        if [ -e $prefix.pdf ] ; then
-            echo "$prefix.pdf exist, please remove to decrypt"
-        else
-            cat $i | gpg --decrypt > $prefix.pdf
-        fi
-    done
+    if [ "$#" -eq 1 ] ; then
+        for i in `find . -type f -regex ".*.gpg"` ; do
+            prefix=${i%%".gpg"}
+            if [ -e $prefix.pdf ] ; then
+                echo "$prefix.pdf exist, please remove to decrypt"
+            else
+                cat $i | gpg --decrypt > $prefix.pdf
+            fi
+        done
+    else
+        prefix=${2%%".gpg"}
+        cat $2 | gpg --decrypt > $prefix.pdf
+    fi
 else
     echo $USAGE >&2
     exit 3
